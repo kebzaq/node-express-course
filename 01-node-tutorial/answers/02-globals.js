4cfb268c006522c7f53b4674151d37cf5f5ef395
Kazbek Abraliev <kazbek.abraliev@gmail.com>
2024-10-26 18:02:06 -0400

    week1: files from 1 to 7 are completed

---
+++ 01-node-tutorial/answers/02-globals.js
@@ -0,0 +1,10 @@
+// GLOBALS
+
+process.env.MY_VAR = "Hi, there!";
+
+console.log(process.env.MY_VAR);
+
+console.log(__dirname);
+console.log(__filename);
+// console.log(require);
+console.log(module);
