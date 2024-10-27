e6738798fe8c28d33a003d74a15c3e436466bd6c
Kazbek Abraliev <kazbek.abraliev@gmail.com>
2024-10-27 19:45:58 -0400

    updated 10-fs-sync.js file

--- 01-node-tutorial/answers/10-fs-sync.js
+++ 01-node-tutorial/answers/10-fs-sync.js
@@ -1,9 +1,19 @@
 const { readFileSync, writeFileSync } = require("fs");
 
+console.log("start...");
 // create file
-writeFileSync("./temporary/fileA.txt", `This is sample test LINE 1`, {
+writeFileSync("./temporary/fileA.txt", `This is sync line 1\n`, {
   flag: "a",
 });
+// console.log(readFileSync("./temporary/fileA.txt", "utf8"));
+writeFileSync("./temporary/fileA.txt", `This is sync line 2\n`, {
+  flag: "a",
+});
+// console.log(readFileSync("./temporary/fileA.txt", "utf8"));
+writeFileSync("./temporary/fileA.txt", `This is sync line 3\n`, {
+  flag: "a",
+});
+console.log(readFileSync("./temporary/fileA.txt", "utf8"));
 
 //read the file
-console.log(readFileSync("./temporary/fileA.txt", "utf8"));
+console.log("end!");
