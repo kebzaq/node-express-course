c48dc737058baaf5924a7f1dd3a5930417dceb6e
Kazbek Abraliev <kazbek.abraliev@gmail.com>
2024-10-27 21:32:40 -0400

    11-fs-async completed

--- 01-node-tutorial/answers/11-fs-async.js
+++ 01-node-tutorial/answers/11-fs-async.js
@@ -1,40 +1,45 @@
 // Async
 
-const { readFile, writeFile } = require("fs");
+const { writeFile } = require("fs");
 
+// write
 console.log("at start...");
-writeFile("./temporary/fileB.txt", `This is Async line 1\n`, (err, result) => {
-  console.log("at point 1");
-  if (err) {
-    console.log("This error happened", err);
-    return;
-  } else {
-    console.log(result);
-    writeFile(
-      "./temporary/fileB.txt",
-      `This is Async line 2\n`,
-      (err, result) => {
-        console.log("at point 2");
-        if (err) {
-          console.log("This error happened", err);
-          return;
-        } else {
-          writeFile(
-            "./temporary/fileB.txt",
-            `This is Async line 3\n`,
-            (err, result) => {
-              console.log("at point 3");
-              if (err) {
-                console.log("This error happened", err);
-                return;
-              } else {
-                console.log(result);
+writeFile(
+  "./temporary/fileB.txt",
+  `This is Async line 1\n`,
+  { flag: "a" },
+  (err, result) => {
+    console.log("at point 1");
+    if (err) {
+      console.log("This error happened", err);
+      return;
+    } else {
+      writeFile(
+        "./temporary/fileB.txt",
+        `This is Async line 2\n`,
+        { flag: "a" },
+        (err, result) => {
+          console.log("at point 2");
+          if (err) {
+            console.log("This error happened", err);
+            return;
+          } else {
+            writeFile(
+              "./temporary/fileB.txt",
+              `This is Async line 3\n`,
+              { flag: "a" },
+              (err, result) => {
+                console.log("at point 3");
+                if (err) {
+                  console.log("This error happened", err);
+                  return;
+                }
               }
-            }
-          );
+            );
+          }
         }
-      }
-    );
+      );
+    }
   }
-});
+);
 console.log("at end!");
