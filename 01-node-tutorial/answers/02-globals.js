cec9b2072a56b945e8e7aae5c87f0e6fb09603f1
Kazbek Abraliev <kazbek.abraliev@gmail.com>
2024-10-31 10:09:59 -0400

    updated 02-globals file from week1 to run env var in cli

--- 01-node-tutorial/answers/02-globals.js
+++ 01-node-tutorial/answers/02-globals.js
@@ -1,12 +1,17 @@
 // GLOBALS
 
-process.env.MY_VAR = "Hi, there!";
+// export MY_VAR = "Hi, there!"; // this should be run in CLI and will be env variable
 
-// console.log(process.env.MY_VAR);
+console.log(process.env.MY_VAR);
+// console.log(process.env);
 
 console.log(__dirname);
 // console.log(__filename);
 // console.log(require);
 // console.log(module);
 // console.log(process);
-console.log(__dirname);
+
+// Global variable:
+MY_VAR = "Hi, there!"; // this is global variable
+console.log(MY_VAR);
+console.log(global);
