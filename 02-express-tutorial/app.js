ef42e616c7adb436e7f668a6a4eb90557f34c94a
Kazbek Abraliev <kazbek.abraliev@gmail.com>
2024-11-14 19:00:46 -0500

    week-3: completed up to task 6

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,20 @@
-console.log('Express Tutorial')
+console.log("Express Tutorial");
+const express = require("express");
+const app = express();
+
+app.use(express.static("./public"));
+
+app.use((req, res) => {
+  res.status(400).send("404 - Page Not Found");
+});
+
+// app.get("/", (req, res) => {
+//   app.res("WOW - you got Express JS");
+// });
+// app.get("test", (req, res) => {
+//   app.res("THIS IS TEST PAGE");
+// });
+
+app.listen(3000, () => {
+  console.log("App is listening port 3000...");
+});
