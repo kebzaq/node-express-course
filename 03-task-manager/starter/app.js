c1842043f79e19193b4351204906527a08c31866
Kazbek Abraliev <kazbek.abraliev@gmail.com>
2024-12-17 22:47:38 -0500

    week-6 assignments completed

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -3,8 +3,11 @@ const app = express();
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
 require("dotenv").config();
+const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 // middleware
+app.use(express.static("./public"));
 app.use(express.json());
 
 app.get("/hello", (req, res) => {
@@ -14,13 +17,16 @@ app.get("/hello", (req, res) => {
 app.use("/api/v1/tasks", tasks);
 app.use("/api/v1/tasks/:id", tasks);
 
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+
 // app.get("/api/v1/tasks"); // get all the tasks
 // app.post("./api/v1/tasks"); // create a new task
 // app.get("./api/v1/tasks/:id"); //get single task
 // app.patch("./api/v1/tasks/:id"); //update task
 // app.delete("./api/v1/tasks/:id"); // delete task
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
